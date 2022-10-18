8f9128438c02ff46e74829fab2a49ba7893a5172
B11-AINF <B11@academico.unab.ad>
2022-10-18 09:51:39 -0500

    Se modifica el mensaje del main

--- src/ListasE.java
+++ src/ListasE.java
@@ -18,7 +18,7 @@ public class ListasE {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hola mundo");        
+        System.out.println("Estimados estudiantes");        
         
         
     }
