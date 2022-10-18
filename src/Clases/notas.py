571cb4b6fda84570ec67bfad054e91954f9ba55c
Julian Santoyo <113941820+juliansant0y0@users.noreply.github.com>
2022-10-18 09:56:21 -0500

    Add files via upload
    Se sube archivo notas

---
+++ src/Clases/notas.py
@@ -0,0 +1,8 @@
+class notas:
+    def __init__(self, laboratorio, proyecto, tareas, trabajo_clase, quiz, parcial):
+        self.laboratorio = laboratorio
+        self.proyecto = proyecto
+        self.tareas = tareas
+        self.trabajo_clase = trabajo_clase
+        self.quiz = quiz
+        self.parcial = parcial
